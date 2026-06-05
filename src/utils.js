d189c2d1fac4708c58e294498130da685dfa292e
SWvs <59214670+sammy0101@users.noreply.github.com>
2026-06-05 21:53:07 +0800

    Add utility functions for IP and JSON handling

---
+++ src/utils.js
@@ -0,0 +1,28 @@
+export function ipToNum(ip) { 
+    return ip.split('.').reduce((a,b) => a * 256 + parseInt(b), 0); 
+}
+
+export function numToIp(n) { 
+    return [(n>>>24)&255, (n>>>16)&255, (n>>>8)&255, n&255].join('.'); 
+}
+
+export function isValidIPv4(ip) { 
+    return /^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/.test(ip); 
+}
+
+export function jsonResponse(data, status = 200) { 
+    return new Response(JSON.stringify(data), { 
+        status, 
+        headers: { 'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*' } 
+    }); 
+}
+
+export function handleCORS() { 
+    return new Response(null, { 
+        headers: { 
+            'Access-Control-Allow-Origin': '*', 
+            'Access-Control-Allow-Methods': 'GET, POST, OPTIONS', 
+            'Access-Control-Allow-Headers': 'Content-Type, Authorization' 
+        } 
+    }); 
+}
